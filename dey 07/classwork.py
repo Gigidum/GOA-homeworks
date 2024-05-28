5a1707b38de5f8bf53caac2a9d55cf8a2bb7a487
Gigidum <gigidumbadze7@gmail.com>
2024-05-28 23:31:12 +0400

    second commit

---
+++ dey 07/classwork.py
@@ -0,0 +1,13 @@
+# boolean - True, False 
+print(44 < 50)
+print(20 < 12)
+
+#boolean
+
+number1 = 99
+number2 = 43
+print(number1 > number2)
+print(number1 < number2)
+
+print(5 == 5)
+print("goA"" == goa)
