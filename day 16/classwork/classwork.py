186b3b74e990b4005dcf01d001d504bfcda1c2d7
Gigidum <gigidumbadze7@gmail.com>
2024-06-28 23:26:01 +0400

    commit

---
+++ day 16/classwork/classwork.py
@@ -0,0 +1,8 @@
+age = int(input("please eanter your age: "))
+
+if age >= 5 and age < 13:
+    print("your are chidren")
+elif >= 13 and age <18:
+    print("you are teenager")
+else:
+    print("you are an adult")
